3279a7f8b56c077d351b9eec837092baff430ac1
ScarletKnight12 <31008590+ScarletKnight12@users.noreply.github.com>
2017-08-23 18:46:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,12 +4,46 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
-
+var articleOne={
+    title:' Whew! Long time since i coded in HTML!',
+    heading: 'Note To me',
+    date: 'aug 8' ,
+    content:`<p> Para 1. 100 ways to be what i want to be.</p>
+    <p> Oh wait, i aldready am what i am, which is what i wanna be. AWESOMEE! :D B) </p>`
+    
+};
+function createTemplate(data)
+{
+    var title=data.title;
+    var heading=data.heading;
+    var content=data.content;
+    var htmlTemplate= ` <html>
+        <title>${title}</title>
+        <body> 
+           This is the fabled article one folks. be sure to read carefully ;)
+        <div> Division 1 I guess....i'll call this cheese *-* </div>
+        <hr/>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <link href="/ui/style.css" rel="stylesheet" />
+        <div class="container">
+        <div> 
+           <a href ="/"> Home </a>
+        </div>
+        <hr/>
+        <h3>${heading}</h3>
+        ${content}
+        </div>
+        </body>   
+    </html>
+    
+    `;  
+    return htmltemplate;
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-one',function (req, res) {
-     res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+     res.send(createTemplate(articleOne));
 });
 
 app.get('/article-two',function (req, res) {
