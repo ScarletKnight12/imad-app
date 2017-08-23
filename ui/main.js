5e6c02966ac41ea4a1d0548bd2766193b7422690
ScarletKnight12 <31008590+ScarletKnight12@users.noreply.github.com>
2017-08-23 20:58:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 console.log('Loaded!');
 var element=document.getElementById('main_text');
 element.innerHTML='yo!';
-var img=document.getElementById('img');
-img.onclick = function(){
-    img.style.marginLeft='100px';
+var img=document.getElementById('madi');
+madi.onclick = function(){
+    madi.style.marginLeft='100px';
 };
