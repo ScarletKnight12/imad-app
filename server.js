ff9b6b78e739e3e1e3d21e9308905da73cc7988e
ScarletKnight12 <31008590+ScarletKnight12@users.noreply.github.com>
2017-09-02 10:35:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,151 +1,17 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var crypto=require('crypto');
-var bodyParser=require('body-Parser');
-var Pool=require('pg').Pool;
-var config={
-    user: 'kaitokudo12',
-    database:'kaitokudo12',
-    host:'db.imad.hasura-app.io',
-    port:'5432',
-    password:process.env.DB_PASSWORD
-};
 
-var app = express();
-
-app.use(morgan('combined'));
-app.use(bodyParser.json());
-var articleOne={
-    title:' Whew! Long time since i coded in HTML!',
-    heading: 'Note To me',
-    date: 'aug 8' ,
-    content:`<p> Para 1. 100 ways to be what i want to be.</p>
-    <p> Oh wait, i aldready am what i am, which is what i wanna be. AWESOMEE! :D B) </p>`
-    
-};
-function createTemplate(data)
-{
-    var title=data.title;
-    var heading=data.heading;
-    var content=data.content;
-    var htmlTemplate= ` <html>
-        <title>${title}</title>
-        <body> 
-           This is the fabled article one folks. be sure to read carefully ;)
-        <div> Division 1 I guess....i'll call this cheese *-* </div>
-        <hr/>
-        <meta name="viewport" content="width=device-width, initial-scale=1"/>
-        <link href="/ui/style.css" rel="stylesheet" />
-        <div class="container">
-        <div> 
-           <a href ="/"> Home </a>
-        </div>
-        <hr/>
-        <h3>${heading}</h3>
-        ${content}
-        </div>
-        </body>   
-    </html>
-    
-    `;  
-    return htmlTemplate;
-}
-
+var app=express();
+app.use(morgon('combined'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-function hash(input,salt){
- var hashed=crypto.pbkdf2Sync(input,salt,10000,512,'sha512');   
- return ["pbkdf2","10000",salt,hashed.toString('hex')].join('$');
-}
-
-app.get('/hash/:input', function(req,res){
-
-    var hashedString=hash(req.params.input,'this-is-some-random-string');
-    res.send(hashedString);
-});
-
-app.post('/create-user',function(req,res){
-    var username=req.body.username;
-    var password=req.body.password;
-    var salt=crypto.randomBytes(128).toString('hex');
-  var dbString =hash(password,salt);  
-  pool.query('INSERT INTO "user"(username,password) VALUES ($1,$2)',[username,dbString],function(err,res){
-   if(err)
-        {
-            res.status(500).send(err.toString());
-        }
-        else{
-         if(result.rows.length===0)
-            res.send("invalid!!");
-        
-        else
-        {
-           var dbString=result.rows[0].password;
-           var salt=dbString.split('$')[2];
-           var hashPassword=hash(password,salt);
-           //match password
-        }
-  }
-      
-  });
-});
-
-var pool= new Pool(config);
-app.get('/test-db',function(req,res){
-    //select 
-    pool.query('SELECT * FROM TEST', function(err,result)
-    {
-        if(err)
-        {
-            res.status(500).send(err.toString());
-        }
-        else{
-            res.send(JSON.stringify(result.rows));
-            
-        }
-    });
-    
-});
-
-
-app.get('/articles/article-one',function (req, res) {
-    var articleName=article.params.articleName;
-    pool.query("SELECT * FROM article WHERE title="+req.params.articleName+"'", function(err,result)
-    {
-       if(err){
-           res.status(500).send(err.toString());
-       } 
-       else{
-           if(result.rows.length===0)
-           {
-               res.status('404').send('Article not found');
-           }
-           else{ 
-               var articleData=result.rows[0];
-               res.send(createTemplate(articleData));
-       }
-       }
-    });
-    
-});
-
-app.get('/article-two',function (req, res) {
-    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
-app.get('/article-three',function (req, res) {
-    res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-}); 
 app.get('/ui/style.css',function(req,res){
    res.sendFile(path.join(__dirname,'ui','style.css'));
 });
-app.get('/ui/main.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
-});
-
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
@@ -154,7 +20,7 @@ app.get('/ui/madi.png', function (req, res) {
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
 
-var port = 80;
-app.listen(port, function () {
+var port = 8080;
+app.listen(8080, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
